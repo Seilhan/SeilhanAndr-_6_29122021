760680bb8acab0f022dac70d1a15d8ae48ab9e6c
Andre Seilhan <seilhanmagnino@gmail.com>
2022-01-10 10:18:17 +0100

    section card__photographer

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,9 +1,12 @@
 function displayModal() {
     const modal = document.getElementById("contact_modal");
-	modal.style.display = "block";
+    modal.style.display = "block";
 }
 
 function closeModal() {
     const modal = document.getElementById("contact_modal");
     modal.style.display = "none";
 }
+
+const selectbox = document.querySelectorAll('.select-box__current')[0];
+document.querySelectorAll('.select-box__option').forEach((btn) => btn.addEventListener("click", () => selectbox.blur()));
