ee24d2790b59864e3ead96d37e80251de710d0aa
Andre Seilhan <seilhanmagnino@gmail.com>
2022-01-14 12:06:36 +0100

    add function on index.js/photographers.js

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,45 +1,62 @@
     async function getPhotographers() {
-        // Penser à remplacer par les données récupérées dans le json
-        const photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+        const response = await fetch("/data/photographers.json");
+        return await response.json();
     }
 
-    async function displayData(photographers) {
+    function displayData(photographers) {
         const photographersSection = document.querySelector(".photographer_section");
 
         photographers.forEach((photographer) => {
             const photographerModel = photographerFactory(photographer);
-            const userCardDOM = photographerModel.getUserCardDOM();
-            photographersSection.appendChild(userCardDOM);
+            photographersSection.appendChild(photographerModel);
         });
     };
+
+
+    function photographerFactory(data) {
+        const li = document.createElement('li');
+        const photo = data.portrait.replace('.jpg', '');
+
+        li.innerHTML = `<li>
+            <article class="photographer">
+                <a href="photographer.html?id=${data.id}" aria-label="${data.name}">
+                    <div class="photographer__cover">
+                        <img width="100" src="/assets/photographers/${photo}-xxlight.jpg" alt="${data.name}">
+                    </div>
+                    <h2 class="photographer__title" tabindex="0">${data.name}</h2>
+                </a>
+                    <div class="photographer__infos" tabindex="0">
+                        <div class="photographer__infos__local text--primary">${data.city}, ${data.country}</div>
+                        <div class="photographer__infos__tagline">${data.tagline}</div>
+                        <div class="photographer__infos__price text--grey">${data.price}€/jour</div>
+                    </div>
+            </article>
+        </li>`;
+
+
+        // li.innerHTML = '<article class="photographer">' +
+        //     '<a href="photographer.html?id=' + data.id + '" aria-label="' + data.name + '">' +
+        //     '<div class="photographer__cover">' +
+        //     '<img width="100" src="/assets/photographers/' + photo + '-xxlight.jpg" alt="' + data.name + '">' +
+        //     '</div>' +
+        //     '<h2 class="photographer__title" tabindex="0">' + data.name + '</h2>' +
+        //     '</a>' +
+        //     '<div class="photographer__infos" tabindex="0">' +
+        //     '<div class="photographer__infos__local text--primary">' + data.city + ' , ' + data.country + '</div>' +
+        //     '<div class="photographer__infos__tagline">' + data.tagline + '</div>' +
+        //     '<div class="photographer__infos__price text--grey">' + data.price + '€/jour</div>' +
+        //     '</div>' +
+        //     '</article>';
+
+        return li
+
+    }
+
 
     async function init() {
         // Récupère les datas des photographes
         const { photographers } = await getPhotographers();
         displayData(photographers);
     };
-    
+
     init();
-    
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,62 @@
 //Mettre le code JavaScript lié à la page photographer.html
+
+function setProfilInfo(data) {
+    //console.log(data);
+
+    document.querySelector('.photographer__title').textContent = data.name;
+    document.querySelector('.photographer__infos__local').textContent = `${data.city}  ${data.country}`;
+    document.querySelector('.photographer__infos__tagline').textContent = data.tagline;
+
+    const photo = data.portrait.replace('.jpg', '');
+    document.querySelector('.photographer__cover').innerHTML = `<img width="100" src="assets/photographers/${photo}-xxlight.jpg" alt="${data.name}">`;
+
+}
+
+function setMedias(medias) {
+    // console.log(medias);
+    const cards = document.querySelector('.cards__list');
+    medias.forEach(media => {
+
+        const card = document.createElement('li');
+        card.setAttribute('class', 'card__list card__list--picture');
+
+        const image = media.image ? media.image.replace('.jpg', '') : media.video.replace('.mp4', '');
+
+        card.innerHTML = `<li>
+            <img class="card__list--cover" width="100" src="assets/photos/${media.photographerId}/${image}-light.jpg" tabindex="0" alt="">
+             <div class="card__list--content">
+                 <h2 class="card__list--content--title" tabindex="0">${media.title}</h2>
+                 <div class="card__list--content--like">
+                     <label id="${media.id}" for="${media.id}" class="like__counter">${media.likes}</label>
+                     <i class="fas fa-heart"></i>
+                 </div>
+            </div>
+         </li>`;
+
+        cards.appendChild(card);
+
+    });
+
+    // document.querySelector('.card__list--content--title').textContent = medias.title;
+
+}
+
+async function getPhotographers() {
+    const id = new URLSearchParams(window.location.search).get('id');
+
+    const response = await fetch("/data/photographers.json");
+    const { photographers, media } = await response.json();
+
+    //console.log(media);
+
+    const photographer = photographers.find(el => el.id == id);
+    const medias = media.filter(el => el.photographerId == id);
+
+    //console.log(medias);
+
+    setProfilInfo(photographer);
+    setMedias(medias);
+
+}
+
+getPhotographers();
